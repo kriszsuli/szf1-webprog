e3ea586d07b0809d8528bf67119304cf07e254d6
Chlebik Krisztofer <susutoo@liba.lol>
2025-05-19 12:26:21 +0200

    Add files via upload

--- 20250512/post/script.js
+++ 20250512/post/script.js
@@ -1,20 +1,87 @@
 const container = document.querySelector(".container");
 
+document.getElementById('submitPost').addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  const postTitle = document.getElementById('postTitle').value;
+  const message = document.getElementById('message').value;
+  const username = document.getElementById('username').value;
+  const captchaResponse = grecaptcha.getResponse();
+
+  if (!captchaResponse) {
+      alert("Töltse ki a reCAPTCHA-t!");
+      return;
+  }
+
+  if (message && username) {
+      const postElement = document.createElement('div');
+      postElement.className = 'post';
+
+      const title = document.createElement('h3');
+      title.innerText = postTitle;
+
+      const content = document.createElement('p');
+      content.innerText = message;
+
+      const user = document.createElement('div');
+      user.className = 'user';
+
+      const image = document.createElement('img');
+      image.src = "img/default.jpg";
+
+      const usernameSpan = document.createElement('span');
+      usernameSpan.innerText = username;
+
+      user.appendChild(image);
+      user.appendChild(usernameSpan);
+      
+      postElement.appendChild(title);
+      postElement.appendChild(content);
+      postElement.appendChild(user);
+      
+      document.querySelector('.container').prepend(postElement);
+      
+      document.getElementById('postTitle').value = '';
+      document.getElementById('message').value = '';
+      document.getElementById('username').value = '';
+      grecaptcha.reset();
+  }
+});
+
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
-  .then((json) => {
-    for (el in json) {
-        const post = document.createElement("div");
-        post.className = "post";
+  .then((posts) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((users) => {
+        posts.forEach((post) => {
+          const postElement = document.createElement("div");
+          postElement.className = "post";
 
-        const title = document.createElement("h3");
-        title.innerText = json[el].title;
+          const title = document.createElement("h3");
+          title.innerText = post.title;
 
-        const content = document.createElement("p");
-        content.innerText = json[el].body;
-        
-        post.appendChild(title);
-        post.appendChild(content);
-        container.appendChild(post);
-    }
+          const content = document.createElement("p");
+          content.innerText = post.body;
+          
+          const user = document.createElement("div");
+          user.className = "user";
+
+          const image = document.createElement("img");
+          image.src = "img/default.jpg";
+
+          const username = document.createElement("span");
+          const userData = users.find(u => u.id === post.userId);
+          username.innerText = userData ? userData.username : "Anonymous";
+
+          user.appendChild(image);
+          user.appendChild(username);
+          
+          postElement.appendChild(title);
+          postElement.appendChild(content);
+          postElement.appendChild(user);
+          
+          container.appendChild(postElement);
+        });
+      });
   });
